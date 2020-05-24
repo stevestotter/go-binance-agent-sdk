0cdc1b10cf46f4cb67c713de1043cd02b4be40f0
Steve Stotter <steve.stotter64@googlemail.com>
2020-05-24 16:07:41 +0100

    Added ELK stack logging with docker-compose

--- agent/agent.go
+++ agent/agent.go
@@ -1,8 +1,11 @@
 package agent
 
 import (
-	"log"
+	"fmt"
 	"stevestotter/go-binance-trader/feeder"
+	"strconv"
+
+	"github.com/rs/zerolog/log"
 )
 
 type Agent struct {
@@ -19,12 +22,14 @@ type MarketListener interface {
 func (a *Agent) Start() {
 	tChan, err := a.Feed.Trades()
 	if err != nil {
-		log.Fatalf("error on reading trades: %s", err)
+		log.Fatal().Err(err).
+			Msg("error on reading trades")
 	}
 
 	eChan, err := a.Feed.BookUpdates()
 	if err != nil {
-		log.Fatalf("error on reading book updates: %s", err)
+		log.Fatal().Err(err).
+			Msg("error on reading book updates")
 	}
 
 	go func() {
@@ -38,10 +43,54 @@ func (a *Agent) Start() {
 	}
 }
 
+// OnTradeUpdate just logs for now
 func (a *Agent) OnTradeUpdate(t feeder.Trade) {
-	log.Printf("Trade received: %+v", t)
+	price, err := strconv.ParseFloat(t.Price, 64)
+	if err != nil {
+		log.Error().Err(err).
+			Msg("Unable to convert price to float")
+	}
+
+	quantity, err := strconv.ParseFloat(t.Quantity, 64)
+	if err != nil {
+		log.Error().Err(err).
+			Msg("Unable to convert quantity to float")
+	}
+
+	log.Debug().
+		Float64("price", price).
+		Float64("quantity", quantity).
+		Str("event_type", t.Type).
+		Int("trade_id", t.ID).
+		Int("buyer_order_id", t.BuyerOrderID).
+		Int("seller_order_id", t.SellerOrderID).
+		Msg("trade")
 }
 
+// OnBookUpdate just logs for now
 func (a *Agent) OnBookUpdate(e feeder.Event) {
-	log.Printf("Update received: %+v", e)
+	price, err := strconv.ParseFloat(e.Price, 64)
+	if err != nil {
+		log.Error().Err(err).
+			Msg("Unable to convert price to float")
+	}
+
+	quantity, err := strconv.ParseFloat(e.Quantity, 64)
+	if err != nil {
+		log.Error().Err(err).
+			Msg("Unable to convert quantity to float")
+	}
+
+	if quantity == 0 {
+		log.Debug().
+			Float64("price", price).
+			Str("event_type", fmt.Sprintf("%s_removed", e.Type)).
+			Msg("book removal")
+	} else {
+		log.Debug().
+			Float64("price", price).
+			Float64("quantity", quantity).
+			Str("event_type", e.Type).
+			Msg("book update")
+	}
 }
